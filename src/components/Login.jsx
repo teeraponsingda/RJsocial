f5e836c65933b5a175473d6b32b9837e9b391e0b
Teerapon Singad <Teeraponsingda1999@gmail.com>
2019-12-26 15:21:51 +0700

    เเก้ไขข้อความหน้า Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,7 +5,7 @@ class Login extends Component {
     render() {
         return (
             <div>
-                <h1>เข้าสู่ระบบ</h1>
+                <h1>เข้าสู่ระบบของธีรพล</h1>
             </div>
         );
     }
